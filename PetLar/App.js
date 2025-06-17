a69e3ef03c8ada20088eca81074df51d0889d005
Pedro Henrique do Nascimento Urbano <pedrohnurbano@gmail.com>
2025-06-17 13:55:09 -0300

    Importação de Atualização
    - Importação com as devidas modificações referentes às telas em si;
    - "Screens" > Renomeação de telas e arquivos específicos.

--- PetLar/App.js
+++ PetLar/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer }        from '@react-navigation/native'      ;
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Home from './Screens/home';
-import Login from './Screens/login';
+import Home             from './Screens/home'            ;
+import Login            from './Screens/login'           ;
 import Cadastro_Usuario from './Screens/cadastro_usuario';
 import Pagina_Principal from './Screens/pagina_principal';
+import Adocao           from './Screens/adocao'          ;
+import Gerenciamento    from './Screens/gerenciamento'   ;
 
 const Stack = createNativeStackNavigator();
 
@@ -18,10 +20,12 @@ export default function App() {
           headerShown: false, // Remove o header padrão para manter o design
         }}
       >
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="CadastroUsuario" component={Cadastro_Usuario} />
-        <Stack.Screen name="PaginaPrincipal" component={Pagina_Principal} />
+        <Stack.Screen name="Home"             component={Home}             />
+        <Stack.Screen name="Login"            component={Login}            />
+        <Stack.Screen name="Cadastro_Usuario" component={Cadastro_Usuario} />
+        <Stack.Screen name="Pagina_Principal" component={Pagina_Principal} />
+        <Stack.Screen name="Adocao"           component={Adocao}           />
+        <Stack.Screen name="Gerenciamento"    component={Gerenciamento}    />
       </Stack.Navigator>
     </NavigationContainer>
   );
